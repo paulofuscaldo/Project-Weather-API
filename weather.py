738ede7e05e662ef2798934ee2be9db35817b81d
paulofuscaldo <75786663+paulofuscaldo@users.noreply.github.com>
2024-04-14 18:15:31 +0100

    Tested: working

--- weather.py
+++ weather.py
@@ -4,32 +4,25 @@ city = input('Enter a city: ')
 
 units = 'metric'
 unitsSymbols = {
-
-'imperial': '˚F',
-
-'metric': '˚C',
-
-'kelvin': 'K'
-
+    'imperial': '˚F',
+    'metric': '˚C',
+    'kelvin': 'K'
 }
 
 try:
-    response = requests.get(('http://api.openweathermap.org/data/2.5/weather?q=' + city + 
-                            '&appid=476b94193414be09d482c9ee0af011e8&units='+units).json())
-    
-currentTemperature = response['main']['temp']
-minTemperature = response['main']['temp_min']
-humidity = response['main']['humidity']
-maxTemperature = response['main']['temp_max']
-feelsLikeTemperature = response['main']['feels_like']
-description = response['weather'][0]['description']
+    response = requests.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=476b94193414be09d482c9ee0af011e8&units='+units).json()
 
-print('In the city of ' + city.capitalize() + ':')  
-print('the current temperature is ' + str(currentTemperature) + unitsSymbols[units] + ' but feels like ' + str(feelsLikeTemperature) + unitsSymbols[units])
-print('the weather is ' + description)
-print('the current humidity level is ' + str(humidity) + '%')
+    currentTemperature = response['main']['temp']
+    minTemperature = response['main']['temp_min']
+    humidity = response['main']['humidity']
+    maxTemperature = response['main']['temp_max']
+    feelsLikeTemperature = response['main']['feels_like']
+    description = response['weather'][0]['description']
 
-except :
+    print('In the city of ' + city.capitalize() + ':')  
+    print('the current temperature is ' + str(currentTemperature) + unitsSymbols[units] + ' but feels like ' + str(feelsLikeTemperature) + unitsSymbols[units])
+    print('the weather is ' + description)
+    print('the current humidity level is ' + str(humidity) + '%')
 
-print('city not found')
-      
+except:
+    print('city not found')
