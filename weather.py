372c8fb11a39c54a978d4ee94ff6890930e8f4d4
paulofuscaldo <75786663+paulofuscaldo@users.noreply.github.com>
2024-04-14 17:52:15 +0100

    current temperature = response main/temp

--- weather.py
+++ weather.py
@@ -16,3 +16,5 @@ unitsSymbols = {
 try:
     response = requests.get(('http://api.openweathermap.org/data/2.5/weather?q=' + city + 
                             '&appid=476b94193414be09d482c9ee0af011e8&units='+units).json())
+    
+ currentTemperature = response['main']['temp']   
